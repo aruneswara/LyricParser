bbc846d79977a07e0d2753a49d86d494aa0ebb36
aruneswara <aruneswara@icloud.com>
2020-12-28 16:39:47 -0600

    other folders added

--- main.py
+++ main.py
@@ -1,16 +1,79 @@
-# coding=utf-8
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-GENIUS_API_TOKEN='k9lIVkBW2OaRoRUZNdeE3L4q0LHOaI7Qaao0LyBoYQXp4JALUIIH302h0KSVZsht'
+GENIUS_API_TOKEN = 'k9lIVkBW2OaRoRUZNdeE3L4q0LHOaI7Qaao0LyBoYQXp4JALUIIH302h0KSVZsht'
 
 
-# Press the green button in the gutter to run the script.
+# Get artist object from Genius API
+def request_artist_info(artist_name, page):
+    base_url = 'https://api.genius.com'
+    headers = {'Authorization': 'Bearer ' + GENIUS_API_TOKEN}
+    search_url = base_url + '/search?per_page=10&page=' + str(page)
+    data = {'q': artist_name}
+    response = requests.get(search_url, data=data, headers=headers)
+    return response
+
+
+# Get Genius.com song url's from artist object
+def request_song_url(artist_name, song_cap):
+    page = 1
+    songs = []
+
+    while True:
+        last = len(songs)
+        response = request_artist_info(artist_name, page)
+        json = response.json()
+        # Collect up to song_cap song objects from artist
+        song_info = []
+        for hit in json['response']['hits']:
+            if artist_name.lower() in hit['result']['primary_artist']['name'].lower():
+                song_info.append(hit)
+
+        # Collect song URL's from song objects
+        for song in song_info:
+            url = song['result']['url']
+            songs.append(url)
+
+        if (len(songs) == last):
+            break
+        else:
+            page += 1
+
+    print('Found {} songs by {}'.format(len(songs), artist_name))
+    return songs
+
+
+# Scrape lyrics from a Genius.com song URL
+def scrape_song_lyrics(url):
+    page = requests.get(url)
+    from bs4 import BeautifulSoup
+    html = BeautifulSoup(page.text, 'html.parser')
+    lyrics = html.find('div', class_='lyrics').get_text()
+    # remove identifiers like chorus, verse, etc
+    lyrics = re.sub(r'[\(\[].*?[\)\]]', '', lyrics)
+    # remove empty lines
+    lyrics = os.linesep.join([s for s in lyrics.splitlines() if s])
+    return lyrics
+
+
+# Loop through URLs
+def write_lyrics_to_file(artist_name, song_count):
+    f = open('lyrics/' + artist_name.lower() + '.txt', 'wb')
+    urls = request_song_url(artist_name, song_count)
+    for url in urls:
+        lyrics = scrape_song_lyrics(url)
+        f.write(lyrics.encode("utf8"))
+    f.close()
+    num_lines = sum(1 for line in open('lyrics/' + artist_name.lower() + '.txt', 'rb'))
+    print('Wrote {} lines to file from {} songs'.format(num_lines, song_count))
+
+
 if __name__ == '__main__':
-    import bs4 as bs
-    import urllib.request
+    import requests
     import os
     import re
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    a = open("artists.txt", "r")
+    Lines = a.readlines()
+    for x in Lines:
+        if (x[:-2][3:] == "Drake"):
+            write_lyrics_to_file(x[:-2][3:], 1000)
+            break
+        write_lyrics_to_file((x[:-2]), 5)
